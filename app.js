cad603f70ca615337675e9a58b604be27d146721
Taylor Laury <tlaury6@yahoo.com>
2021-08-27 21:14:48 -0400

    Created JavaScript Variables

--- app.js
+++ app.js
@@ -4,7 +4,19 @@ console.log(
 );
 
 // Exercise 1
-
+const firstName = "Taylor";
+let lastName = "Laury";
+var myAge = 27;
 // Exercise 2
+let fullName = firstName + " "+ lastName;
+let templateFullName = `${firstName} ${lastName}`;
+console.log("fullName: ", fullName);
+console.log("templateFullName: ", templateFullName);
 
 // Exercise 3
+let myStory;
+let city = "New York City";
+let pastime = "playing The Sims, eating, watching YouTube videos and playing with my dog"
+
+myStory = `Hello! My name is ${fullName}. I live in ${city}. I enjoy ${pastime}.`
+console.log (myStory);
